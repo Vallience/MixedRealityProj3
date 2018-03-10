a084055c62d2bff0f753345659f0951557c139ba
kenyonnick <jusisgood45@gmail.com>
2018-03-10 14:21:34 -0500

    Updated crop function to match project requirements and added EXIF info

--- nickApartment.m
+++ nickApartment.m
@@ -1,9 +1,33 @@
+
+%% Load Images
 pathSlug = 'JesseApartment/originalSpherePhotographs/';
 sceneImg = imread(strcat(pathSlug, 'scene.jpg'));
 
 for i=1:7
-    exposures{i} = imread(strcat(pathSlug, 'im', num2str(i), '.jpg'));
-    [cropped{i}, rects{i}] = imcrop(exposures{i});
-    imwrite(cropped{i}, strcat('JesseApartment/croppedSphereImages/im', ...
-        num2str(i), '.jpg'));
+    origPath = strcat(pathSlug, 'im', num2str(i), '.jpg');
+    origImages{i} = imread(origPath);
 end
+
+%% Crop Images
+
+[cropped{i}, rectangle] = imcrop(origImages{2});
+rectangle(4) = rectangle(3);
+
+for i=1:7
+    [cropped{i}, rectangle] = imcrop(origImages{i}, rectangle);
+    
+    cropPath = strcat('JesseApartment/croppedSphereImages/im', num2str(i), '.jpg');
+    imwrite(cropped{i}, cropPath);
+end
+
+%% Get EXIF Info and construct vector B
+for i=1:7
+    origPath = strcat(pathSlug, 'im', num2str(i), '.jpg');
+    info = imfinfo(origPath);
+    B(i) = info.DigitalCamera.ExposureTime;
+end
+
+%% Setup sparse matrix Z
+
+
+
