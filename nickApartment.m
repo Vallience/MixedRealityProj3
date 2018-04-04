99670d78fe4ef8d7a9b6eb32949c2ea44ff07a92
Nicklas Kenyon <jusisgood45@gmail.com>
2018-04-04 13:19:38 -0400

    Started HDR composition

--- nickApartment.m
+++ nickApartment.m
@@ -29,3 +29,20 @@ end
 
 %% Setup sparse matrix Z
 
+% do this for each image to get each color channel as a vector
+
+im1 = im2double(cropped{1});
+[height, width, depth] = size(im1);
+
+im1r = reshape(im1(:,:,1), height*width, 1);
+im1g = reshape(im1(:,:,2), height*width, 1);
+im1b = reshape(im1(:,:,3), height*width, 1);
+
+% use gsolve to get the log of the input illumination
+% [g, le] = gsolve(Z, B, 1)
+
+% get the HDR image by exponentiating the log
+% E = exp(le)
+
+
+    
